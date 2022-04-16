e10d3d4908d103ff67b21485ac436e1582b35ac7
AlinaShashko <75527092+AlinaShashko@users.noreply.github.com>
2022-04-17 00:59:38 +0300

    Initial commit

---
+++ src/aura/footer/footerController.js
@@ -0,0 +1,13 @@
+({
+    modelClick: function (cmp, event, helper) {
+        alert("You clicked: " + event.getSource().get("v.label"));
+    },
+
+    dealersClick: function (cmp, event, helper) {
+        alert("You clicked: " + event.getSource().get("v.label"));
+    },
+
+    serviceClick: function (cmp, event, helper) {
+        alert("You clicked: " + event.getSource().get("v.label"));
+    }
+})
---
+++ src/aura/map/mapController.js
@@ -0,0 +1,37 @@
+({
+    init: function (cmp, event, helper) {
+        try{
+            helper.helperMethod(cmp, "c.infoForMap" , function (response) {
+            let mapMarkers = [];
+            for (let i = 0; i < response.length; i++) {
+                let dealer = response[i];
+                let markers ={
+                  location: {
+                    Latitude: dealer.Latitude__c,
+                    Longitude: dealer.Longitude__c
+                  },
+                  icon: "standard:account",
+                  title: dealer.Name,
+                  description:  (
+                           '<p> Phone: ' + dealer.Sales_Department_Phone__c + '</p>' +
+                           '<p> Working Hours: ' + dealer.Working_Hours__c + '</p>' + 
+                           '<p> Address: ' + dealer.Address__c + '</p>'
+                        ),
+                      icon: 'standard:location'
+                }
+                  mapMarkers.push(markers);
+          }
+	
+            cmp.set('v.mapMarkers', mapMarkers);
+            cmp.set('v.center', {
+                				location: {
+                                    		City: 'Minsk'}
+            });
+            cmp.set('v.zoomLevel', 11);
+            cmp.set('v.markersTitle', 'Dealer centers LADA');
+           });
+        } catch (exception ) {
+          			System.debug(LoggingLevel.ERROR, "Exception" + exception.getMessage());
+        }
+    }
+});
